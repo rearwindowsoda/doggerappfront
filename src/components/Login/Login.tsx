6f1aba70cca663aeb2b0f7dc8e6dcec0aef130f0
MateuszOwczarek <mowczarek602@gmail.com>
2022-07-11 16:35:45 +0200

    Started working on the first component - login form

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './Login.css'
+export const Login = () => {
+    return(
+    <main className="form-signin w-100 m-auto">
+        <form>
+            <img className="logo-image" src="/logo.png" alt="Doggerapp logo"/>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div className="form-floating">
+                <input type="text" className="form-control" id="floatingInput" placeholder="Your login" />
+                <label htmlFor="floatingInput">Login</label>
+            </div>
+            <div className="form-floating">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                <label htmlFor="floatingPassword">Password</label>
+            </div>
+
+
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </form>
+    </main>
+    )
+}
