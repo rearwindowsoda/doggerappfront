117398112d54f9c39752c86cd9a9b50b4e98dcb6
MateuszOwczarek <mowczarek602@gmail.com>
2022-07-11 16:45:02 +0200

    Started implementing react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import {Login} from "./components/Login/Login";
 
 function App() {
-    return <Login/>
-
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
+
 
 export default App;
