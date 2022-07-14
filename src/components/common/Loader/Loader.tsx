d13cd928162c3a47867e010f16bcc1b222659515
MateuszOwczarek <mowczarek602@gmail.com>
2022-07-14 15:18:22 +0200

    Added loader

---
+++ src/components/common/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import './Loader.css'
+
+export const Loader =  () => {
+    return <div className="container">
+        <div className="lds-ripple">
+        <div></div>
+        <div></div>
+    </div></div>
+
+}
