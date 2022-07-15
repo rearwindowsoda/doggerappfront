6898e56b375242ad236fe0e3bd93de80a886f3c0
MateuszOwczarek <mowczarek602@gmail.com>
2022-07-15 09:03:36 +0200

    Added top ten posts route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {Register} from "./components/Register/Register";
 
 import {NavBar} from "./components/common/NavBar/NavBar";
 import {Home} from "./components/Home/Home";
+import {TopTenPosts} from "./components/TopTen/TopTenPosts";
 function App() {
     return (
         <BrowserRouter>
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Register/>}/>
+                <Route path="/top10" element={<TopTenPosts/>}/>
 
             </Routes>
         </BrowserRouter>
